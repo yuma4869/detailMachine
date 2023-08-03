97d8fd2a32beae4ff57cb676a243bc18b4be452f
yuma4869 <tttyyy76@outlook.jp>
2023-08-03 12:37:49 +0900

    Update index.php

--- index.php
+++ index.php
@@ -4,37 +4,63 @@ function pickupmachineLanguage($string){
 $searchString = ":"; // 検索する文字列
 $length = 12; // 取り出す文字数
 
-$startPosition = 0; // 検索開始位置 
+$startPosition = 0; // 検索開始位置
 
 $vector = "";
 
 while (($position = strpos($string, $searchString, $startPosition)) !== false) {
     // セミコロンが見つかった場合
     $extractedString = substr($string, $position + 1, $length);
-    echo "aaa $extractedString";
     $vector .= $extractedString;
-    echo $extractedString . PHP_EOL;
-    
     $startPosition = $position + 1; // 次の検索を開始する位置を更新
 }
 $int = 13;
 $outputlang = substr($vector,$int);
 $vector = preg_replace('/\s+/', '', $outputlang);
-echo $vector;
 return $vector;
 }
 
 function hexToBinary($hexString) {
-    echo "hexstring $hexString end";
     $binaryString = '';
     $length = strlen($hexString);
-    echo "len $length";
     for ($i = 0; $i < $length; $i++) {
         $binaryString .= str_pad(base_convert($hexString[$i], 16, 2), 4, '0', STR_PAD_LEFT);
         $binaryString .= " ";
     }
     return $binaryString;
 }
+?>
+<?php
+        if (isset($_POST["submit"])){
+            $file_path = "example.c";
+            $code = $_POST["code"];
+            $fp = fopen($file_path, 'w');
+            if ($fp !== false) {
+                fwrite($fp, $code);
+                fclose($fp);
+            } else {
+                echo "<div class='test'>ファイルをオープンできませんでした。</div>";
+            }
+            shell_exec("gcc -g -o example example.c");
+            $gdb_output = shell_exec('gdb -q -batch -x command.gdb example');
+
+            // 改行コードをHTMLの改行タグに変換して表示
+            $file_path = "binary.s";
+            $code = $gdb_output;
+            $fp = fopen($file_path, 'w');
+            if ($fp !== false) {
+                fwrite($fp, $code);
+                fclose($fp);
+            } else {
+                echo "ファイルをオープンできませんでした。";
+            }
+            shell_exec("gcc -nostartfiles -o binary binary.s");
+            $objdump = shell_exec("objdump -d example");
+            $sixteenlang = pickupmachineLanguage($objdump);
+            $intsixteen = str_replace(":","",$sixteenlang);
+            $machineLanguage = hexToBinary($intsixteen);
+
+        }
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -50,12 +76,27 @@ function hexToBinary($hexString) {
         background-color: #333333;
         color: #00FF00;
     }
+    input[type=submit]{
+        display:block;
+        cursor: pointer;
+        width:90%;
+        margin:10px auto;
+        padding:10px;
+        background-color:rgba(244, 192, 104,0.8);
+        color:white;
+        font-size: 1em;
+        border:0;
+        border-radius:10px;
+    }
+    input[type=submit]:hover {
+        background-color: grey;
+    }
     .explain {
         text-align:center;
     }
-    
+
     textarea {
-        overflow-y: auto;
+       overflow-y: auto;
     background-color: rgb(249, 251, 254);
     margin-top: 24px;
     display: block;
@@ -100,7 +141,7 @@ color:red;
 </style>
 <body>
 <div class="explain">
-    <h1>detailMachine | yuma4869</h1>
+    <h1>machineConverter | yuma4869</h1>
     <h3>このサイトでは機械で実行されている「機械語（マシン語）」を見ることができます</h3>
     <h3>0と1だけの世界、楽しんでね！</h3>
     <p>01111001 01110101 01101101 01100001 00110100 00111000 00110110 00111001</p>
@@ -108,10 +149,11 @@ color:red;
 
 <div class="inputCode">
     <h2>C言語のコードを入力してください</h2>
+    <p>コンパイルエラーが出ない正しいコードで入力してください</p>
     <form method="post" action="index.php">
     <div class="flex" style="display: flex;">
     <textarea name="code" id="inputCode" oninput="updatePredictions()" onkeydown="checkKeyPress(event)"></textarea>
-    <div id="result" class="log" style="color:00FF00"><?php echo $machineLanguage ?></div>
+    <div id="result"><?php echo $machineLanguage ?></div>
     </div>
     <div id="predictions" style="height: 1.2em;position: absolute;"></div>
         <input type="submit" value="送信" name="submit">
@@ -122,44 +164,14 @@ color:red;
 </div>
 <div class="log">
 <?php
-        if (isset($_POST["submit"])){
-            $file_path = "example.c";
-            $code = $_POST["code"];
-            $fp = fopen($file_path, 'w');
-            if ($fp !== false) {
-                fwrite($fp, $code);
-                fclose($fp);
-                echo "<div class='test'>ファイルにコードを保存しました。</div>";
-            } else {
-                echo "<div class='test'>ファイルをオープンできませんでした。</div>";
-            }
-            shell_exec("gcc -g -o example example.c");
-            $gdb_output = shell_exec('gdb -q -batch -x command.gdb example');
-
-            // 改行コードをHTMLの改行タグに変換して表示
-            echo "aaaaa $gdb_output end";
             echo nl2br(htmlspecialchars($gdb_output));
-            $file_path = "binary.s";
-            $code = $gdb_output;
-            $fp = fopen($file_path, 'w');
-            if ($fp !== false) {
-                fwrite($fp, $code);
-                fclose($fp);
-                echo "ファイルにコードを保存しました。";
-            } else {
-                echo "ファイルをオープンできませんでした。";
-            }
-            shell_exec("gcc -nostartfiles -o binary binary.s");
-            $objdump = shell_exec("objdump -d example");
             echo $objdump;
-            echo "ahahaha";
-            $sixteenlang = pickupmachineLanguage($objdump);
-            echo "$sixteenlang";
-            $intsixteen = str_replace(":","",$sixteenlang);
-            echo "$intsixteen";
-            $machineLanguage = hexToBinary($intsixteen);
-            
-        }
+            echo $sixteenlang;
+            echo $intsixteen;
+            unlink("example.c");
+            unlink("example");
+            unlink("binary.s");
+
 ?>
 </div>
 </body>
